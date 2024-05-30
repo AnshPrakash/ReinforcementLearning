26ac0691e018e9683cde0771d61f324fab145094
Ansh <anshprakash@gmail.com>
2024-05-30 22:45:05 +0200

    run policy and value iteration

--- Assignments/HW1_HB/Ex2_DP_HB.py
+++ Assignments/HW1_HB/Ex2_DP_HB.py
@@ -22,9 +22,10 @@ def visualize_value_matrices(V, label):
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     ### YOUR CODE HERE ###
     # reshape the value matrices
+    Vc = V.reshape((2, 10, 10))
 
-    V1 =
-    V2 =
+    V1 = Vc[0]
+    V2 = Vc[1]
 
     ######################
 
@@ -60,9 +61,10 @@ def visualize_policy(pi, label):
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     ## YOUR CODE HERE ###
     # reshape the policy matrices
+    pi_c = pi.reshape((2, 10, 10))
 
-    pi1 = 
-    pi2 = 
+    pi1 = pi_c[0]
+    pi2 = pi_c[1]
 
     ######################
 
@@ -159,6 +161,43 @@ def policy_iteration_left(prob, reward, gamma, iterations):
 
     # modify the existing policy iteration algorithm from MushroomRL
     # start with a left initialized policy
+
+    n_states = prob.shape[0]
+    n_actions = prob.shape[1]
+
+    policy = np.ones(n_states, dtype=int)*2
+    value = np.zeros(n_states)
+
+    changed = True
+    while changed:
+        p_pi = np.zeros((n_states, n_states))
+        r_pi = np.zeros(n_states)
+        i = np.eye(n_states)
+
+        for state in range(n_states):
+            action = policy[state]
+            p_pi_s = prob[state, action, :]
+            r_pi_s = reward[state, action, :]
+
+            p_pi[state, :] = p_pi_s.T
+            r_pi[state] = p_pi_s.T.dot(r_pi_s)
+
+        value = np.linalg.solve(i - gamma * p_pi, r_pi)
+
+        changed = False
+
+        for state in range(n_states):
+            vmax = value[state]
+            for action in range(n_actions):
+                if action != policy[state]:
+                    p_sa = prob[state, action]
+                    r_sa = reward[state, action]
+                    va = p_sa.T.dot(r_sa + gamma * value)
+                    if va > vmax and not np.isclose(va, vmax):
+                        policy[state] = action
+                        vmax = va
+                        changed = True
+
 
     ######################
 
